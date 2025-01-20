6d8f156f1c8114190c286ecc6c0c40f613491eb9
Dariush Alipour <drsh.alipour@gmail.com>
2025-01-20 09:06:01 +0100

    remove tsimport.mjs

--- packages/core/scripts/build.mjs
+++ packages/core/scripts/build.mjs
@@ -1,5 +1,11 @@
-import { build } from "../src/index.js";
-import buildConfig from "../zoboz.config.js";
+import * as url from "url";
+
+// @ts-expect-error It is still not available in the types, since it is only a Release Candidate
+const { register } = await import("module");
+register("./bin/deps/tsload.mjs", url.pathToFileURL("./"));
+
+const { build } = await import("../src/index.js");
+const buildConfig = await import("../zoboz.config.js");
 
 // @ts-expect-error
-build(buildConfig, true);
+build(buildConfig.default, true);
--- packages/core/scripts/tsimport.mjs
+++
@@ -1,8 +0,0 @@
-import { register } from "module";
-import * as path from "path";
-import { pathToFileURL } from "url";
-
-register(
-	path.resolve(import.meta.dirname, "../bin/deps/tsload.mjs"),
-	pathToFileURL("."),
-);
