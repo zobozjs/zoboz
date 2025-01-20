c6d0054fa5358e2c6502faa9f3d4d186a84edbdd
Dariush Alipour <drsh.alipour@gmail.com>
2025-01-20 21:25:11 +0100

    prevent unwanted package-lock.json during docker tests

--- scripts/test.docker.sh
+++ scripts/test.docker.sh
@@ -6,7 +6,7 @@ for node_version in 12 14 16 18 20 22; do
   for ts_version in 4 5; do
     docker run -it --rm -v $(pwd):/app -w /app node:$node_version bash -c "
       npm install -g typescript@$ts_version &&
-      cd /app/packages/core && npm link &&
+      cd /app/packages/core && npm link --global-style &&
       cd /app/level-ones/core-bundler && npm run build &&
       cd /app/level-ones/core-node10 && npm run build &&
       cd /app/level-ones/core-node16 && npm run build &&
