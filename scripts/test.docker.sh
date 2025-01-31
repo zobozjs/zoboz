53fc3085ca66e7e4daf9a0e8f98a2a4251730823
Dariush Alipour <drsh.alipour@gmail.com>
2025-01-31 22:54:00 +0100

    fix test docker npm auth

--- scripts/test.docker.sh
+++ scripts/test.docker.sh
@@ -8,23 +8,26 @@ npm -w @zoboz/core run build
 
 for node_version in 14 16 18 20 22; do
   for ts_version in 4 5; do
-    ./verdaccio/run.sh --start
+    ./verdaccio/run.sh --start >/dev/null
     npm -w @zoboz/core publish --registry http://localhost:4873
     docker run -it --rm --network zoboz-net -v $(pwd)/tests:/app -v $(pwd)/temp:/app/temp -w /app node:$node_version bash -c "
+      export NPM_CONFIG_REGISTRY=http://verdaccio:4873 &&
+      npm config set '//verdaccio:4873/:_authToken' 'OGQ5NDgxYjAwOWZkNzFhZmRlZGZiOTNjZGZhY2QyYTY6MTEwZDlkOTQ0YWVjYWViNjIzYzE5NTg4M2I1NzUx' &&
+      npm config set registry http://verdaccio:4873 &&
       echo '⏳ Testing (node: $node_version, ts: $ts_version)' >> /app/temp/test.docker.sh.results &&
       npm install -g typescript@$ts_version &&
-      cd /app/level-ones/core-bundler && rm -rf node_modules package-lock.json && npm i --registry http://verdaccio:4873 && npm run build && npm publish --registry http://verdaccio:4873 &&
-      cd /app/level-ones/core-node10 && rm -rf node_modules package-lock.json && npm i --registry http://verdaccio:4873 && npm run build && npm publish --registry http://verdaccio:4873 &&
-      cd /app/level-ones/core-node16 && rm -rf node_modules package-lock.json && npm i --registry http://verdaccio:4873 && npm run build && npm publish --registry http://verdaccio:4873 &&
-      cd /app/level-ones/esbuild-bundler && rm -rf node_modules package-lock.json && npm i --registry http://verdaccio:4873 && npm run build && npm publish --registry http://verdaccio:4873 &&
-      cd /app/level-ones/esbuild-node10 && rm -rf node_modules package-lock.json && npm i --registry http://verdaccio:4873 && npm run build && npm publish --registry http://verdaccio:4873 &&
-      cd /app/level-ones/esbuild-node16 && rm -rf node_modules package-lock.json && npm i --registry http://verdaccio:4873 && npm run build && npm publish --registry http://verdaccio:4873 &&
-      cd /app/level-twos/bundler && rm -rf node_modules package-lock.json && npm i --registry http://verdaccio:4873 && npm run test && npm run typecheck &&
-      cd /app/level-twos/node10 && rm -rf node_modules package-lock.json && npm i --registry http://verdaccio:4873 && npm run test && npm run typecheck &&
-      cd /app/level-twos/node16 && rm -rf node_modules package-lock.json && npm i --registry http://verdaccio:4873 && npm run test npm run typecheck &&
+      cd /app/level-ones/core-bundler && rm -rf node_modules package-lock.json && npm i && npm run build && npm publish &&
+      cd /app/level-ones/core-node10 && rm -rf node_modules package-lock.json && npm i && npm run build && npm publish &&
+      cd /app/level-ones/core-node16 && rm -rf node_modules package-lock.json && npm i && npm run build && npm publish &&
+      cd /app/level-ones/esbuild-bundler && rm -rf node_modules package-lock.json && npm i && npm run build && npm publish &&
+      cd /app/level-ones/esbuild-node10 && rm -rf node_modules package-lock.json && npm i && npm run build && npm publish &&
+      cd /app/level-ones/esbuild-node16 && rm -rf node_modules package-lock.json && npm i && npm run build && npm publish &&
+      cd /app/level-twos/bundler && rm -rf node_modules package-lock.json && npm i && npm run test && npm run typecheck &&
+      cd /app/level-twos/node10 && rm -rf node_modules package-lock.json && npm i && npm run test && npm run typecheck &&
+      cd /app/level-twos/node16 && rm -rf node_modules package-lock.json && npm i && npm run test npm run typecheck &&
       echo '🍃 Done (node: $node_version, ts: $ts_version)' >> /app/temp/test.docker.sh.results ||
       echo '🌶️ Failed (node: $node_version, ts: $ts_version)' >> /app/temp/test.docker.sh.results
     "
-    ./verdaccio/run.sh --stop
+    ./verdaccio/run.sh --stop >/dev/null
   done
 done
