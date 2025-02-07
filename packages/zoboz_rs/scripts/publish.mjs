8e75e044fb126ce30d9ecc4ddc51973b7becb530
Dariush Alipour <drsh.alipour@gmail.com>
2025-02-07 21:49:59 +0100

    add snippets

--- packages/zoboz_rs/scripts/publish.mjs
+++ packages/zoboz_rs/scripts/publish.mjs
@@ -1,6 +1,34 @@
 // "os": "aix", "darwin", "freebsd", "linux", "openbsd", "sunos", "win32"
 // "cpu": "x64", "arm", "arm64", "ia32", "mips", "mipsel", "ppc", "ppc64", "s390", "s390x"
 
+// rustup target add aarch64-unknown-linux-musl
+// rustup target add x86_64-unknown-linux-musl
+// rustup target add aarch64-apple-darwin
+// rustup target add x86_64-apple-darwin
+// rustup target add aarch64-pc-windows-msvc
+// rustup target add x86_64-pc-windows-msvc
+
+// cargo zigbuild --release --target aarch64-apple-darwin
+// cargo zigbuild --release --target x86_64-apple-darwin
+// cargo zigbuild --release --target aarch64-unknown-linux-musl
+// cargo zigbuild --release --target x86_64-unknown-linux-musl
+// cargo zigbuild --release --target aarch64-pc-windows-msvc
+// cargo zigbuild --release --target x86_64-pc-windows-msvc
+
+// `pwd`/target/aarch64-apple-darwin/release/zoboz_rs
+// `pwd`/target/x86_64-apple-darwin/release/zoboz_rs
+// docker run -it --rm --platform linux/arm64 -v `pwd`/target/aarch64-unknown-linux-musl/release:/app ubuntu /app/zoboz_rs
+// docker run -it --rm --platform linux/amd64 -v `pwd`/target/x86_64-unknown-linux-musl/release:/app ubuntu /app/zoboz_rs
+// ${pwd}/target/aarch64-pc-windows-msvc/release/zoboz_rs.exe
+// ${pwd}/target/x86_64-pc-windows-msvc/release/zoboz_rs.exe
+
+// cp target/aarch64-apple-darwin/release/zoboz_rs packages/zoboz_rs_darwin_arm64/zoboz_rs_darwin_arm64
+// cp target/x86_64-apple-darwin/release/zoboz_rs packages/zoboz_rs_darwin_x64/zoboz_rs_darwin_x64
+// cp target/aarch64-unknown-linux-musl/release/zoboz_rs packages/zoboz_rs_linux_arm64/zoboz_rs_linux_arm64
+// cp target/x86_64-unknown-linux-musl/release/zoboz_rs packages/zoboz_rs_linux_x64/zoboz_rs_linux_x64
+// cp target/aarch64-pc-windows-msvc/release/zoboz_rs.exe packages/zoboz_rs_win32_arm64/zoboz_rs_win32_arm64.exe
+// cp target/x86_64-pc-windows-msvc/release/zoboz_rs.exe packages/zoboz_rs_win32_x64/zoboz_rs_win32_x64.exe
+
 import fs from "fs";
 import path from "path";
 
