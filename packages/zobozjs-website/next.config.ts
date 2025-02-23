022d90b3486439b0a7ad958a9c385ceb63c53921
Dariush Alipour <drsh.alipour@gmail.com>
2025-02-23 18:09:08 +0100

    add nextjs for zobozjs.github.io

---
+++ packages/zobozjs-website/next.config.ts
@@ -0,0 +1,9 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: "export",
+  basePath: process.env.NODE_ENV === "production" ? "" : "",
+  assetPrefix: process.env.NODE_ENV === "production" ? "" : "",
+};
+
+export default nextConfig;
