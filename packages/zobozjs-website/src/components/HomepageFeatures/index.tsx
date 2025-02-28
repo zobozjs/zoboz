dc7a47affe0c68b7968041fb3cac7d40235ae286
Dariush Alipour <drsh.alipour@gmail.com>
2025-02-28 03:37:57 +0100

    improve text-align

--- packages/zobozjs-website/src/components/HomepageFeatures/index.tsx
+++ packages/zobozjs-website/src/components/HomepageFeatures/index.tsx
@@ -53,7 +53,7 @@ function Feature({ title, Svg, description }: FeatureItem) {
 			{/* <div className="text--center">
 				<Svg className={styles.featureSvg} role="img" />
 			</div> */}
-			<div className="text--center padding-horiz--md">
+			<div className="padding-horiz--md">
 				<Heading as="h3">{title}</Heading>
 				<p>{description}</p>
 			</div>
