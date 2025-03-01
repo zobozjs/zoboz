78efe135094920488d424b5208b54d02baafef92
Dariush Alipour <drsh.alipour@gmail.com>
2025-03-01 02:17:47 +0100

    improve docs home page

--- packages/zobozjs-website/src/pages/index.tsx
+++ packages/zobozjs-website/src/pages/index.tsx
@@ -3,6 +3,7 @@ import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import HomepageFeatures from "@site/src/components/HomepageFeatures";
 import Heading from "@theme/Heading";
 import Layout from "@theme/Layout";
+import CodeBlock from "@theme/CodeBlock";
 import clsx from "clsx";
 import type { ReactNode } from "react";
 
@@ -14,17 +15,46 @@ function HomepageHeader() {
 	return (
 		<header className={clsx("hero hero--primary", styles.heroBanner)}>
 			<div className="container">
-				<Heading as="h1" className="hero__title">
-					{siteConfig.title}
-				</Heading>
-				<p className="hero__subtitle">{siteConfig.tagline}</p>
-				<div className={styles.buttons}>
-					<Link
-						className="button button--secondary button--md"
-						to="/docs/intro"
-					>
-						Getting Started
-					</Link>
+				<div className="row">
+					<div className="col col--6 margin-vert--lg items-center justify-center">
+						<div>
+							<Heading as="h1" className="hero__title">
+								{siteConfig.title}
+							</Heading>
+							<p className="hero__subtitle">{siteConfig.tagline}</p>
+							<div className={styles.buttons}>
+								<Link
+									className="button button--secondary button--md"
+									to="/docs/intro"
+								>
+									Getting Started
+								</Link>
+							</div>
+						</div>
+					</div>
+					<div className="col col--6 text--left">
+						<CodeBlock language="typescript">
+							{`
+// zoboz.config.ts
+
+import { BuildConfig, tsc, esbuild } from "@zoboz/core"
+
+export default new BuildConfig({
+  esm: esbuild.esm(), // or tsc.esm()
+  cjs: esbuild.cjs(), // or tsc.cjs()
+  dts: tsc.dts(),
+  srcDir: "./src",
+  distDir: "./dist",
+  exports: {
+    ".": "./src/index.ts",
+  },
+})
+
+// Zoboz will build your package
+// and handle package.json fields
+        `}
+						</CodeBlock>
+					</div>
 				</div>
 			</div>
 		</header>
@@ -38,7 +68,7 @@ export default function Home(): ReactNode {
 			<HomepageHeader />
 			<main>
 				<HomepageFeatures />
-				<hr />
+				<hr className="container margin-bottom--lg" />
 				<LevelsOfControl />
 			</main>
 		</Layout>
