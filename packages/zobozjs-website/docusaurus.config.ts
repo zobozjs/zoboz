972e29f9262c1f024bbd389f9a72c2d1a4a4f4d0
Bruno Paulino <contact@bpaulino.com>
2025-03-13 16:43:27 +0100

    Add custom OpenGraph image (#3)

--- packages/zobozjs-website/docusaurus.config.ts
+++ packages/zobozjs-website/docusaurus.config.ts
@@ -67,8 +67,7 @@ const config: Config = {
 	],
 
 	themeConfig: {
-		// Replace with your project's social card
-		image: "img/docusaurus-social-card.jpg",
+		image: "img/open_graph.jpg",
 		navbar: {
 			logo: {
 				alt: "Zoboz Logo",
