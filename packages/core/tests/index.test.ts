3f8c925329972adfeadd58c088630f4db6a2332d
Dariush Alipour <drsh.alipour@gmail.com>
2025-03-21 23:54:56 +0100

    refactor test setup

--- packages/core/tests/index.test.ts
+++
@@ -1,9 +0,0 @@
-import { loadConfiguration, runCucumber } from "@cucumber/cucumber/api";
-
-async function main() {
-	const { runConfiguration } = await loadConfiguration();
-	const { success } = await runCucumber(runConfiguration);
-	console.log(success);
-}
-
-main();
